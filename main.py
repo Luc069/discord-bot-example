e563a0a0cb3cb27f692382517934f3ce76f0e261
Luc069 <91779803+Luc069@users.noreply.github.com>
2021-10-02 14:35:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import nextcord as discord
 from nextcord.ext import commands
 
-TOKEN = '#YOUR TOKEN'   https://youtu.be/aI4OmIbkJH8 (How to get it)
+TOKEN = 'YOUR TOKEN'   https://youtu.be/aI4OmIbkJH8 (How to get it)
 client = comands.Bot(command_prefix='!')
 
 @client.command()
