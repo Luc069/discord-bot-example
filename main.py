68d6920f2bdb4f01bc476fb472acfd04b766fa79
Luc069 <91779803+Luc069@users.noreply.github.com>
2021-10-03 12:17:10 +0200

    Update main.py
    Just fixed something LOL.

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import nextcord as discord
 from nextcord.ext import commands
 
 TOKEN = 'YOUR TOKEN'   https://youtu.be/aI4OmIbkJH8 (How to get it)
-client = comands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='!')
 
 @client.command()
 async def hello(ctx):
  await ctx.send('hello')
  
- client.run(TOKEN)
+client.run(TOKEN)
